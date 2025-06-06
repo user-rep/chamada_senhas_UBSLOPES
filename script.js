25ad6f8b0ad89dc4a3f6da44e0e4e1698bb8bb09
user-rep <renner_egalon@yahoo.com.br>
2025-06-06 19:05:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -570,13 +570,14 @@ async function chamarSenhaSincronizada(tipo, guiche) {
   const botao = botoes.find(b => b.textContent.includes(`Senha ${contador} -`));
 
   if (botao) {
-  botao.click();
-  firebase.database().ref('ultimaSenhaChamada').set({
-    idColuna: idColuna,
-    numeroSenha: numeroSenha,
-    classeDestaque: classeDestaque,
-    timestamp: Date.now()
-  });
+    botao.click();
+	firebase.database().ref('ultimaSenhaChamada').set({
+  idColuna: idColuna,
+  numeroSenha: numeroSenha,
+  classeDestaque: classeDestaque,
+  timestamp: Date.now()
+});
+
   } else {
     console.error('Botão não encontrado:', `Senha ${contador} - Guichê ${guiche}`);
   }
@@ -603,12 +604,9 @@ function repetirUltimaSenha() {
   if (!ultimaSenhaChamada) return;
 
   const texto = ultimaSenhaChamada.textContent;
+  const isPreferencial = texto.toLowerCase().includes("preferencial");
   const numeroSenha = parseInt(texto.match(/Senha (\d+)/)?.[1], 10);
   const destino = texto.split(" - ")[1];
-
-  // Verifica diretamente a qual coluna o botão pertence
-  const coluna = ultimaSenhaChamada.closest(".coluna");
-  const isPreferencial = coluna?.id?.includes("preferencial");
 
   const textoFalado = isPreferencial
     ? `Senha ${numeroSenha}, preferencial, ${destino}`
