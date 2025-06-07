a95efdefac0dadeecc099d05950d65e818a0992e
user-rep <renner_egalon@yahoo.com.br>
2025-06-07 20:08:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -684,13 +684,24 @@ document.addEventListener("keydown", function (event) {
     esperarSegundoKey('n');
   } else if (tecla === 'p') {
     esperarSegundoKey('p');
-  } else if (tecla === 'enter') {
-    if (enterPressionadoRecentemente) return; // Evita repetição
-	      enterPressionadoRecentemente = true;
-setTimeout(() => {
-      enterPressionadoRecentemente = false;
-    }, 500); // 0,5s de bloqueio para evitar duplo acionamento
-  }
+  else if (tecla === 'enter') {
+  if (enterPressionadoRecentemente || !botaoSelecionado) return;
+
+  enterPressionadoRecentemente = true;
+  setTimeout(() => {
+    enterPressionadoRecentemente = false;
+  }, 500);
+
+  // Bloqueia clique se outro computador estiver falando
+  tentarReservarLocutor().then(podeFalar => {
+    if (!podeFalar) {
+      console.log("Outro computador está falando. Aguarde...");
+      return;
+    }
+
+    botaoSelecionado.click(); // chama a senha selecionada com fala normal
+  });
+}
 });
 
 let botaoSelecionado = null;
